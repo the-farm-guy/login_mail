0cf0dcf963e60d16385bf8a6d564b3da4555199c
thefarmguy011 <thefarmguy011@gmail.com>
2025-04-23 10:04:26 +0530

    initial commit

--- database/model.py
+++ database/model.py
@@ -1,14 +1,3 @@
-# from sqlalchemy import Column, Integer, String
-# from sqlalchemy.orm import declarative_base
-
-# Base = declarative_base()
-
-# class User(Base):
-#     __tablename__ = 'users'
-    
-#     id = Column(Integer, primary_key=True)
-#     username = Column(String, nullable=False, unique=True)
-#     password = Column(String, nullable=False)
 from sqlalchemy import Column, Integer, String, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
 
